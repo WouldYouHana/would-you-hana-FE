c8edee8e8d206e6f10c6d8e1ff038c8946b959b9
heon-kim <vndtka123@gmail.com>
2024-12-27 17:43:44 +0900

    fix: 개인정보 수정 백엔드 연결

--- src/services/bankerMypage.service.ts
+++ src/services/bankerMypage.service.ts
@@ -1,11 +1,11 @@
 import { config } from "../config/config";
 import { request } from "../hoc/request";
-import { BankerInfoResponeDTO } from "../types/dto/mypage.dto";
+import { BankerInfoResponeDTO, BankerInfoUpdateDTO } from "../types/dto/mypage.dto";
 
 const BASE_URL = config.apiUrl;
 
 export const bankerMypageService = {
-    // 질문 스크랩
+    // 행원 개인정보 수정 전 필드에 표시할 기존 정보 불러오기
     getBankerMyPageInfo: (bankerId: number) => {
         return request<BankerInfoResponeDTO>({
             method: 'GET',
@@ -14,6 +14,17 @@ export const bankerMypageService = {
                 bankerId: bankerId.toString()
             }
         });
+    },
+    // 행원 개인정보 수정 제출
+    modifyBankerInfo:(bankerId:number, data:BankerInfoUpdateDTO)=>{
+      return request<BankerInfoResponeDTO>({
+        method: 'PUT',
+        url: `${BASE_URL}/my/bankers/edit/info/submit`,
+        params: {
+            bankerId: bankerId.toString()
+        },
+        data
+    });
     },
     // 행원 프로필 수정
     modifyBankerProfile: (data: FormData) => {
